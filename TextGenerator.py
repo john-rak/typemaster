b5ef9b813ed6a2f4f4f404d3352736a8d720e3b1
John Rak <jrak@agari.com>
2019-12-15 14:08:26 -0800

    update infrastructure

--- TextGenerator.py
+++ TextGenerator.py
@@ -1,4 +1,6 @@
 import os
+from collections import defaultdict
+import random
 
 class TextGenerator:
 
@@ -7,13 +9,15 @@ class TextGenerator:
     word_len = int
     line_len = int
     filetype = str
+    generated_text = list
 
-    def __init__(self, directory='/Users/jrak/Work', filetype='.py', min_length=5, max_length=50, format_out='word'):
+    def __init__(self, directory='/Users/jrak/Work/houston', filetype='.py', min_length=5, max_length=50, format_out='word'):
         self.directory = directory
         self.filetype = filetype
         self.min_length = min_length
         self.max_length = max_length
         self.format_out = format_out
+        self.generated_text = []
 
     def generate_file_paths(self) -> list:
         file_paths = []
@@ -22,17 +26,73 @@ class TextGenerator:
                 filepath = os.path.join(path, file)
                 if os.path.isfile(filepath) and filepath.endswith(self.filetype):
                     file_paths.append(filepath)
-        print(file_paths)
         return file_paths
 
     def is_valid_text(self, text):
         valid = False
         if self.format_out == 'word':
-            valid = text.isascii() and self.min_length < len(text) < self.max_length
+            valid = text.isascii() #and self.min_length < len(text) < self.max_length
         elif self.format_out == 'line':
             valid = text.isascii() and self.min_length < len(text) < self.max_length
 
         return valid
+
+    def scrape_file_text(self, text_data) -> None:
+        if self.format_out == 'word':
+            for line in text_data:
+                for word in line.split():
+                    if self.is_valid_text(word):
+                        self.generated_text.append(word)
+        elif self.format_out == 'line':
+            for line in text_data:
+                line = line.strip()
+                if self.is_valid_text(line):
+                    self.generated_text.append(line)
+
+    def iterate_and_scrape_files(self, file_path_list):
+        filecount = 0
+        for filepath in file_path_list:
+            with open(filepath, 'r') as f:
+                try:
+                    data = f.readlines()
+                    self.scrape_file_text(data)
+                    filecount += 1
+                except FileNotFoundError:
+                    print('Unable to read file.')
+
+        print(f'Read {filecount} files successfully.')
+
+    # ---------------
+    # Word specific logic
+    # ---------------
+    def generate_words_list(self) -> None:
+        assert self.format_out == 'word'
+        file_paths = self.generate_file_paths()
+        self.iterate_and_scrape_files(file_paths)
+
+    def generate_random_string_from_words(self) -> str:
+        randStr = ''
+        while len(randStr) < self.min_length:
+            index = random.randint(0, len(self.generated_text))
+            word = self.generated_text[index]
+            randStr += word + ' '
+
+        return randStr[:-1]
+
+
+
+    # ---------------
+    # Line specific logic
+    # ---------------
+    def generate_lines_list(self) -> None:
+        assert self.format_out == 'line'
+
+        file_paths = self.generate_file_paths()
+        self.iterate_and_scrape_files(file_paths)
+
+
+    def generate_random_line(self) -> str:
+        return random.sample(self.generated_text, k=1)[0]
 
 
 
--- app.py
+++ app.py
@@ -10,9 +10,9 @@ from collections import defaultdict
 import random
 import pickle
 
-test_Generator = TextGenerator()
-test_UI = UI()
-test_StatsTracker = StatsTracker()
+test_generator = any #TextGenerator.TextGenerator()
+test_UI = any #UI.UI()
+test_StatsTracker = any #StatsTracker.StatsTracker()
 
 
 
@@ -40,104 +40,18 @@ def run_basic_lines(line_list):
             print('INCORRECT! Game over...')
             break
 
-def generate_random_basic_words(wordlist, length) -> str:
-    randStr = ''
-    for word in random.sample(wordlist, k=length):
-        if is_valid_word(word):
-            randStr += word + ' '
-        else:
-            word = random.sample(wordlist, k=1)[0]
-            while not is_valid_word(word):
-                word = random.sample(wordlist, k=1)[0]
-            randStr += word + ' '
-    return randStr[:-1]
-
-def is_valid_word(word):
-    return word.isascii() and 2 < len(word) < 10
-
-def generate_random_line(linelist, length) -> str:
-    line = random.sample(linelist, k=1)[0].strip()
-    while not is_valid_line(line):
-        line = random.sample(linelist, k=1)[0].strip()
-    return line
-
-
-def is_valid_line(line):
-    return line.isascii() and 15 < len(line) < 50
-
-def generate_files() -> list:
-    file_paths = []
-    for path, _, files in os.walk('/Users/jrak/Work/'):
-        for file in files:
-            filepath = os.path.join(path, file)
-            if os.path.isfile(filepath) and filepath.endswith('.py'):
-                file_paths.append(filepath)
-    print(file_paths)
-    return file_paths
-
-
-def generate_words() -> dict:
-    WORD_LIST = defaultdict(lambda: 0)
-    file_paths = generate_files()
-
-    for filepath in file_paths:
-        with open(filepath, 'r') as f:
-            try:
-                data = f.readlines()
-                for line in data:
-                    for word in line.split():
-                        WORD_LIST[word] += 1
-                print('Read file.')
-            except FileNotFoundError:
-                print('Unable to read file.')
-    return dict(WORD_LIST)
-
-
-def generate_words_list() -> list:
-    WORD_LIST = []
-    file_paths = generate_files()
-    for filepath in file_paths:
-        with open(filepath, 'r') as f:
-            try:
-                data = f.readlines()
-                for line in data:
-                    for word in line.split():
-                        WORD_LIST.append(word)
-                print('Read file.')
-            except FileNotFoundError:
-                print('Unable to read file.')
-    return WORD_LIST
-    # print(sorted(WORD_LIST.items(),key= lambda x:x[1]))
-
-
-def generate_lines() -> list:
-    LINE_LIST = []
-    file_paths = generate_files()
-    for filepath in file_paths:
-        with open(filepath, 'r') as f:
-            try:
-                data = f.readlines()
-                for line in data:
-                    if line.isascii():
-                        LINE_LIST.append(line)
-                print('Read file.')
-            except FileNotFoundError:
-                print('Unable to read file.')
-    return LINE_LIST
-
 
 def main():
-    line_list = []
-    if os.path.isfile('lines_list.pkl'):
-        with open('lines_list.pkl', 'rb') as file:
-            line_list = pickle.load(file)
-            print('Found pickled file!')
-    else:
-        print('Calling generate_lines()...')
-        line_list = sorted(generate_lines())
-        with open('lines_list.pkl', 'wb') as file:
-            pickle.dump(word_list, file)
-    run_basic_lines(line_list)
+    test_gen = TextGenerator.TextGenerator(format_out='word', min_length=40)
+    test_gen.generate_words_list()
+    for i in range(5):
+        print(test_gen.generate_random_string_from_words())
+
+def main2():
+    test_gen = TextGenerator.TextGenerator(format_out='line')
+    test_gen.generate_lines_list()
+    for i in range(5):
+        print(test_gen.generate_random_line())
 
 
 if __name__ == '__main__':
